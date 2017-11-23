94a956fb4c675581afee1e3573baaef8d3ebf9a4
JP <jpyepezimc@gmail.com>
2017-11-24 03:31:39 +1300

    added files

---
+++ Source/UE4PlayerInput/UE4PlayerInputGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4PlayerInputGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4PLAYERINPUT_API AUE4PlayerInputGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
